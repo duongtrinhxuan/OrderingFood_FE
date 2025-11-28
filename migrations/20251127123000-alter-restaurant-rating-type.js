aac6eb775276d64e5ae41867648757f3f0593167
duongtrinhxuan <22520310@gm.uit.edu.vn>
2025-11-28 18:53:56 +0700

    migration

---
+++ migrations/20251127123000-alter-restaurant-rating-type.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up() {
+    // Migration no longer needed because rating stays as INTEGER.
+  },
+
+  async down() {},
+};
